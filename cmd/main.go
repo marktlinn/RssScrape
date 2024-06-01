506ba0964667adabd1ab5e9dd86cc374934309d1
marktlinn <marktlinn@gmail.com>
2024-06-02 00:12:12 +0200

    add: initial goose migration file + postgres drivers

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/marktlinn/RssScrape/server"
 )
 
@@ -12,6 +14,18 @@ func main() {
 	if port == "" {
 		log.Fatalln("failed to find port: PORT env variable must be set")
 	}
+
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatalln("failed to connect to DB: DB env variable must be set")
+	}
+
+	conn, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatalf("failed to connect to Postgres instance: %s\n", err)
+	}
+
+	log.Printf("connected to postgres: %v\n", conn)
 
 	server.Server(port)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.2
 
 require github.com/go-chi/chi/v5 v5.0.12
 
-require github.com/go-chi/cors v1.2.1
+require (
+	github.com/go-chi/cors v1.2.1
+	github.com/lib/pq v1.10.9
+)
